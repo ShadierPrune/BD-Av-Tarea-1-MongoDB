af4de69fec6d780b0f02234e5a8d6e548a5fd6f1
ShadierPrune <shadierprune@gmail.com>
2026-09-08 13:40:03 -0300

    subiendo cositas

---
+++ README.md
@@ -0,0 +1,4 @@
+Nombre: Felipe Santiago Parra Díaz
+Rol: 202373568-k
+Rut: 21.758.067-6
+Paralelo: 200
---
+++ generador_actores.py
@@ -0,0 +1,43 @@
+
+# Datos para generar 1 persona
+nombres = [
+    "Daniel Day-Lewis", "Hugh Grant", "Julia Roberts", "Ed Oxenbould",
+    "Steve Carell"
+    ]
+nacionalidades =[
+    "Chile","España","Argentina","Perú","Reino Unido","Francia","Corea",
+    "Italia","Japón","China","Filipinas", "Estados Unidos de América",
+    "México","Canada","Portugal","Brasil","Venezuela","Andorra",
+    "Rusia","India","Tailandia","Indonesia","Australia","Malasia",
+    "Mongolia","Egipto","Polonia","Paises Bajos"
+    ]
+fechas_de_nacimientos = []
+edades = []
+patrimonio = []
+idiomas = [
+    "Inglés","Español","Japonés","Coreano","Mandarin","Italiano","Francés",
+    "Portugués","Ruso","Hindi","Tailandés","Filipino","Indonesio","Malayo",
+    "Mongol","Árabe","Polaco","Neerlandés"
+    ]
+
+# Datos para generar a los personajes de 1 persona
+
+nombres_personajes = [
+    "Goku","Gohan","Naruto","Sakura","Harry Potter","Sung Jin-woo",
+    "Ben Tennyson", "Hawkeye","Simon Belmont", "Trevor Belmont",
+    "Ritcher Belmont", "Kim Gong-Ja", "Peter Parker", "Mr. Darcy",
+    "Elizabeth Bennet", "William Thacker", "Anna Scott", "Jonesy Jones",
+    "Alexander Cooper", "Ben Cooper", "Kelly Cooper", "Vegeta"
+    ]
+titulos_obras = [
+    "Dragon Ball Z","Naruto","Ben 10", "El último de los Mohicanos",
+    "CastleVania", "Soy leyenda", "Cazador Suicida de Clase SSS",
+    "Solo Leveling", "Misión Imposible III", "Rápidos y Furiosos 8",
+    "Spider-Man", "Detective Pikachu", "Orgullo y Prejuicio",
+    "Notting Hill","Fortnite",
+    "Alexander y un día terrible, horrible, malo... ¡Muy malo!"
+    ]
+tipo_de_produccion = ["Profesional", "Independiente"]
+rol = ["Principal","Secundario","Extra"]
+cantidad_apariciones = []
+generos_de_produccion = []
